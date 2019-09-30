ef962b90d4c4620ae56fca7bde2ebee5b328f1b7
Dipak Akhade <dipak.a.akhade1291@gmail.com>
2019-10-01 00:00:33 +0530

    initial commit

---
+++ Order Monato/DAL/OrderMonatoContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Order_Monato.Models;
+
+namespace Order_Monato.DAL
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+
+    public class OrderMonatoContext : DbContext
+    {
+        public OrderMonatoContext() : base("OrderMonatoContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Order Monato/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Order_Monato.MvcApplication" Language="C#" %>
